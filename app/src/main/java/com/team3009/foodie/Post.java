39fedca7b381c29372cfe33ad8e858d47124a6f6
Moshimane <Joy.Moshimane@gmail.com>
2017-08-25 09:57:36 +0200

    Upload picture and description

--- app/src/main/java/com/team3009/foodie/Post.java
+++ app/src/main/java/com/team3009/foodie/Post.java
@@ -1,21 +1,8 @@
 package com.team3009.foodie;
 
-import android.content.Intent;
-import android.location.Location;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.team3009.foodie.Serve;
 
 /**
  * Created by Zanele on 8/22/2017.
@@ -31,7 +18,9 @@ public class Post {
     String BodyField ;
 
 
-
+    public Post(String title, String body, Double latitude, Double longitude, String uri){
+        post(title, body, latitude, longitude, uri);
+    }
 
     public void post(String title, String body, Double latitude, Double longitude, String uri) {
         // Get the Firebase node to write the data to
