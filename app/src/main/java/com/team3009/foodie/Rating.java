89e6e5c5eaac00c8275d673039b9511baaa9ff3a
ml_mafisa <mafisalawrence@yahoo.com>
2017-10-30 09:37:56 +0200

    third working ver.

---
+++ app/src/main/java/com/team3009/foodie/Rating.java
@@ -0,0 +1,18 @@
+package com.team3009.foodie;
+
+/**
+ * Created by Zanele on 29-Oct-17.
+ */
+
+public class Rating {
+
+    String rating;
+
+    public Rating(){
+
+    }
+
+    public Rating(String rating){
+        this.rating = rating;
+    }
+}
